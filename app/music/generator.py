802a76df865ecc9175b6f170a887df2e1f3135cb
DenMitter <57758gfk@gmail.com>
2023-10-29 16:22:46 +0200

    edit music function, fix play/pause button

--- app/music/generator.py
+++
@@ -1,6 +0,0 @@
-import random
-num = input('login ')
-pas = ''
-for x in range(16): #Количество символов (16)
-    pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ')) #Символы, из которых будет составлен пароль
-print(pas)
